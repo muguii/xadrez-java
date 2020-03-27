0bfc470525cee060f47577638935944e98329b51
Muriel Aurelio <murielaaurelio@gmail.com>
2020-03-27 17:13:21 -0300

    Movendo peças

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,34 @@
 package application;
 
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		ChessMatch chessMatch = new ChessMatch();
-		UI.printBoard(chessMatch.getPieces());
+		Scanner scan = new Scanner(System.in);
+		
+		
+		while (true) {
+			UI.printBoard(chessMatch.getPieces());
+			System.out.println();
+			System.out.print("Origem: ");
+			ChessPosition source = UI.readChessPosition(scan);
+			
+			System.out.println();
+			System.out.print("Target: ");
+			ChessPosition target = UI.readChessPosition(scan);
+			
+			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			System.out.println(capturedPiece);
+			
+			
+		}
 	
 		
 		
--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -20,7 +20,7 @@ public class ChessPiece extends Piece{
 		return color;
 	}
 
-	
+
 	
 	
 	
